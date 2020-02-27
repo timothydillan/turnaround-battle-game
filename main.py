2cdacabf6a9427aa0874caa0e65d98b96149bfff
timothydillan <timothydillan@users.noreply.github.com>
2020-02-27 18:05:45 +0800

    Code comments

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#Initialize libraries needed for the game.
 import pygame, sys, time
 from pygame.locals import *
 from setup import *
 
 def __main__():
+    #Play the background music
     bgMusic()
     run = True
     while run:
@@ -11,17 +13,22 @@ def __main__():
                 pygame.quit()
                 sys.exit()
                 run = False
+        #Create a new frame
         screen.blit(Variables.bgImage, [0, 0])
+        #Create main menu buttons
         MenuButton("New Game", 325, 400, 150, 30, Color.white, Color.Gray, "play")
         MenuButton("Load Game", 325, 450, 150, 30, Color.white, Color.Gray, "load")
         MenuButton("Option", 325, 500, 150, 30, Color.white, Color.Gray, 'option')
         MenuButton("Quit", 325, 550, 150, 30,  Color.white, Color.Gray, "quit")
-
+        #Render PSB Battle Game as the title
         Title('PSB Battle Game', Variables.textX, Variables.textY)
+        #Draw players for the main menu (UI Purposes)
         Player()
+        #Fully update the screen so that we can see everything we rendered before.
         pygame.display.flip()
         Variables.fpsClock.tick(Variables.FPS)
 
-
+#https://stackoverflow.com/questions/419163/what-does-if-name-main-do/20158605#20158605
+#only run the code if its the entry point to the program
 if __name__ == '__main__':
     __main__()
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
+#Initialize libraries needed for the game.
 import pygame, sys, time
 from pygame.locals import *
 from main import *
 
+#Initialize pygame and the pygame mixer and set screen size (width, height).
 pygame.mixer.pre_init(44100, -16, 2, 4096)
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 class Variables():
+    #Declare variables in a class so that its easier to call and cleaner.
     FPS = 30
     fpsClock = pygame.time.Clock()
     font = pygame.font.Font('font/SHPinscher-Regular.otf', 90)
@@ -38,53 +41,73 @@ class Color():
     darkGray = (64, 64, 64)
 
 class Button():
+  #Class for creating and maintaining unique buttons for a number of different purposes
   def assignImage(self, picture):
+  #function for handling the assignment of an image to each individual button object
     self.rect = picture.get_rect()
   def setCoords(self, x,y):
+  #Function for handling the assignment of coordinates for each individual button
+  #object
     self.rect.topleft = x,y
   def drawButton(self, picture):
+  #Function for handling drawing the actual button on the screen
     screen.blit(picture, self.rect)
   def pressed(self,mouse):
+  #Function for determining whether or not a mouse click is inside a button object
     if self.rect.collidepoint(mouse) == True:
       return True
 
 def Title(text, x, y):
+    #A function used for creating a title text.
     text = Variables.font.render(text, True, (255, 255, 255))
     screen.blit(text, (x,y))
 
 def clickSound():
+    #A function used to play a sound that we define.
     sound = pygame.mixer.Sound('Sound/clickSound.ogg')
-    sound.play()
+    #sound.play()
 
 def MenuButton(msg, x, y, w, h, ic, ac, action=None):
+    #Similar to the button class, this function also creates a button,
+    #but this is mainly for the menu buttons, and the other button is mainly for
+    #character choosing etc
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-
+    #Get mouse position
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        #Change button color when hovering
         pygame.draw.rect(screen, ic, (x, y, w, h))
+        #Check if a button is clicked and there is no action running
         if click[0] == 1 and action != None:
             if action == "play":
+                #if the new game button is clicked, run game loop
                 clickSound()
                 gameLoop()
             elif action == 'load':
+                #if the load game button is clicked, load text file from saved game and continue game
                 clickSound()
             elif action == 'option':
+                #if the option game button is clicked, show options on reducing volume, etc.
                 clickSound()
             elif action == "quit":
+                #if the quit game button is clicked, quit the game.
                 clickSound()
                 pygame.quit()
                 sys.exit()
     else:
         pygame.draw.rect(screen, ac, (x,y, w, h))
-    textSurf_1, textRect_1 = textObject(msg, Variables.fontText)
-    textRect_1.center = ((x + (w/2)), (y + (h/2)))
-    screen.blit(textSurf_1, textRect_1)
+    textSurf, textRect = textObject(msg, Variables.fontText)
+    textRect.center = ((x + (w/2)), (y + (h/2)))
+    screen.blit(textSurf, textRect)
 
 def bgMusic():
+    #A function used to play a song that we define.
     bgsound = pygame.mixer.music.load('Sound/backgroundmusic.ogg')
+    #Loop the music.
     pygame.mixer.music.play(-1)
 
 def Player():
+    #Show players on the main menu (UI Purposes)
     screen.blit(Variables.warriorImg, (Variables.player1X, Variables.player1Y))
     screen.blit(Variables.tankerImg, (Variables.player2X, Variables.player2Y))
     screen.blit(Variables.warriorEnemyImg, (Variables.aI1X, Variables.aI1Y))
@@ -95,22 +118,27 @@ def textObject(text, font):
     return textSurface, textSurface.get_rect()
 
 def Dialog(text, x, y):
+    #This function renders a text.
     text = Variables.fontText.render(text, True, (255, 255, 255))
     screen.blit(text, (x, y))
 
 def startScreen():
-    startscreen = False
-    while startscreen == False:
+    #This function is used for the "click on anywhere" part of the game
+    start = False
+    while start == False:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == MOUSEBUTTONDOWN:
-                startscreen = True
+                #Check if the user clicks anywhere and set the boolean to true
+                start = True
 
 def gameLoop():
+    #Start game by creating a new frame (by loading a bg image)
     screen.blit(Variables.bgImage, [0, 0])
 
+    #Declare units as buttons so that users can pick them.
     warriorButton = Button()
     warriorButton.assignImage(Variables.warriorImg)
     warriorButton.setCoords(200, 350)
@@ -119,41 +147,66 @@ def gameLoop():
     tankButton.assignImage(Variables.tankerEnemyImg)
     tankButton.setCoords(500, 340)
 
+    #Draw the buttons declared before.
     warriorButton.drawButton(Variables.warriorImg)
     tankButton.drawButton(Variables.tankerEnemyImg)
 
+    #Show some welcoming Tex
     Dialog('Welcome to the game!', 100, 100)
     Dialog('Choose 3 units to start your adventure!', 100, 150)
+
+    #Update the display so that it can continue to the new frame.
     pygame.display.update()
+
+    #Start choosing process
+    #Declare variables needed in the choosing process
     warriorChoice = 0
     tankChoice = 0
+    #while warriorchoice + tankchoice is less than 3, we create
     while warriorChoice + tankChoice < 3:
+        #a nested loop (for loop) that loops through the possible pygame event
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            #Check if the mouse is clicking
             elif event.type == MOUSEBUTTONDOWN:
+                #Now get the position of the mouse
                 mouse = pygame.mouse.get_pos()
+                #and check if the warriorbutton is pressed by the mouse
                 if warriorButton.pressed(mouse) == True:
+                  #add warriorChoice by 1 to make sure that the loop stops if the units reaches 3.
                   warriorChoice += 1
+                  #Declare a text that contains a value of how many warriors the player has in his/her team,
+                  #so that he/she knows how many unit does he/she have
                   warriorText = Variables.fontText.render('%d Warrior' % (warriorChoice,), 1, (255, 255, 255))
+                  #make a click sound so the user knows that they've clicked on the button
                   clickSound()
+                #now check if the tankbutton is pressed by the mouse
                 if tankButton.pressed(mouse) == True:
+                  #add tankChoice by 1 to make sure that the loop stops if the units reaches 3.
                   tankChoice += 1
+                  #Declare a text that contains a value of how many tanker the player has in his/her team,
+                  #so that he/she knows how many unit does he/she have
                   tankerText = Variables.fontText.render('%d Tanker' % (tankChoice,), 1, (255, 255, 255))
+                  #make a click sound so the user knows that they've clicked on the button
                   clickSound()
-
-
+    #after the loop ends, create a new frame
     screen.blit(Variables.bgImage, [0, 0])
     Dialog("CLICK ANYWHERE TO CONTINUE...", 100, 100)
-    #we need to check if the warriorchoice is more than 0 so that if the
-    #user chooses 3 tanker/warrior it doesnt crash
+    #now lets actually render the text from the while loop before
     if (warriorChoice > 0):
+        #the if statement is needed because if the player chooses 3 tankers,
+        #that means the warriortext will be null, which will result in a crash
         screen.blit(warriorText, [260, 320])
         screen.blit(Variables.warriorImg, [280, 350])
     if (tankChoice > 0):
+        #the if statement is needed because if the player chooses 3 tankers,
+        #that means the tankertext will be null, which will result in a crash
         screen.blit(tankerText, [433, 320])
         screen.blit(Variables.tankerEnemyImg, [443, 350])
-
+    #Update the display again so
+    #we can continue and remove the
+    #elements before (not directly, we're just removing the rendered things)
     pygame.display.update()
     startScreen()
